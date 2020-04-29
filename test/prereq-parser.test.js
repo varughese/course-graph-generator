e6a089e84276156b2acbb2a930ab1a4378fb90e2
Mathew Varughese <mat.varughese3@gmail.com>
2020-04-29 00:35:02 -0400

    why did i commit node modules what

--- test/prereq-parser.test.js
+++ test/prereq-parser.test.js
@@ -1,6 +1,25 @@
+const pegjsparser = require("../lib/courses/parser/parser").parse;
 const parse = require("../lib/courses/parser/prereq-parser");
 const { prereqsFailedToParse } = require("../lib/courses/parser/prereq-fixer");
 
+test("peg js parser is correct", () => {
+	const cases = [
+		["PREQ: BIOENG 1310 and MATH 0240 and MATH 0290 CREQ: cs1504", {
+			"prereq": {
+			   "and": [
+				  "BIOENG1310",
+				  "MATH0240",
+				  "MATH0290"
+			   ]
+			},
+			"coreq": "CS1504"
+		}],
+		["PREQ: BIOENG 1310; PLAN bioengineer ", {
+			"prereq": "BIOENG1310",
+			"coreq": null
+		}],
+	].forEach(([text, output]) => expect(pegjsparser(text)).toEqual(output))
+})
 
 test("properly sets prereq tree", () => {
 	const course = {
@@ -11,7 +30,10 @@ test("properly sets prereq tree", () => {
 	expect(parsed).toBe(0);
 	expect(course.reqs).toEqual({
 		text: "PREQ: CS0445 (MIN GRADE C)",
-		tree: "CS0445"
+		tree: {
+			"coreq": null,
+			"prereq": "CS0445",
+		}
 	});
 })
 
@@ -32,12 +54,14 @@ test("prereqsFailedToParse correctly detects misinterpreted parsed tree", () => 
 	const course = {
 		id: "MING1555",
 		reqs: {
-			text: "This is messed up",
+			text: "PLAN Engineering",
 			tree: null
 		}
 	}
 	
-	expect(prereqsFailedToParse(course)).toBe(true);
+	expect(prereqsFailedToParse(course)).toBe(false);
 	course.reqs.text = '';
 	expect(prereqsFailedToParse(course)).toBe(false);
+	course.reqs.text = 'PREQ: failed to parse';
+	expect(prereqsFailedToParse(course)).toBe(true);
 });
