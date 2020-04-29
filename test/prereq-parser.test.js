9a2c0b591c18a40b57eeff6e571f4d48a493a3be
Mathew Varughese <mat.varughese3@gmail.com>
2020-04-28 23:36:31 -0400

    fixed weird cli input bug

--- test/prereq-parser.test.js
+++ test/prereq-parser.test.js
@@ -1,4 +1,5 @@
-const parse = require("../lib/courses/prereq_parser/prereq-parser");
+const parse = require("../lib/courses/parser/prereq-parser");
+const { prereqsFailedToParse } = require("../lib/courses/parser/prereq-fixer");
 
 
 test("properly sets prereq tree", () => {
@@ -7,20 +8,36 @@ test("properly sets prereq tree", () => {
 		prereq: ["PREQ: CS0445 (MIN GRADE C)"]
 	}
 	const parsed = parse(course);
-	expect(parsed.reqs).toEqual({
+	expect(parsed).toBe(0);
+	expect(course.reqs).toEqual({
 		text: "PREQ: CS0445 (MIN GRADE C)",
 		tree: "CS0445"
 	});
-  })
+})
 
-  test("properly sets prereq tree to null if cannot parse string", () => {
+test("properly sets prereq tree to null if cannot parse string", () => {
 	const course = {
 		id: "CS1501",
 		prereq: ["PREWRONGQ: CS0445 (MIN GRADE C)"]
 	}
 	const parsed = parse(course);
-	expect(parsed.reqs).toEqual({
+	expect(parsed).toBe(1);
+	expect(course.reqs).toEqual({
 		text: "PREWRONGQ: CS0445 (MIN GRADE C)",
 		tree: null
 	});
-  })
+})
+
+test("prereqsFailedToParse correctly detects misinterpreted parsed tree", () => {
+	const course = {
+		id: "MING1555",
+		reqs: {
+			text: "This is messed up",
+			tree: null
+		}
+	}
+	
+	expect(prereqsFailedToParse(course)).toBe(true);
+	course.reqs.text = '';
+	expect(prereqsFailedToParse(course)).toBe(false);
+});
